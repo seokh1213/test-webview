8caf8036321f94234b8d90a711e86bcdba9e028c
tony.1213 <tony.1213@kakaoent.com>
2021-12-20 15:21:52 +0900

    test

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,22 +1,63 @@
 import { Link, useNavigate } from "react-router-dom";
 
 function TestPage() {
+  const baseUrl = "/test-webview/result";
+
   const navigate = useNavigate();
   const homeHandler = () => {
     navigate("/");
   };
   const availableHandler = () => {
-    navigate("/?layoutId=1&seatId=1&available=true&startTime=10:00&endTime=19:00&date=2021-12-15");
+    navigate(
+      "/?layoutId=1&seatId=1&available=true&startTime=10:00&endTime=19:00&date=2021-12-15"
+    );
   };
   const unavailableHandler = () => {
-    navigate("/?layoutId=1&seatId=1&available=false&startTime=10:00&endTime=19:00&date=2021-12-15");
+    navigate(
+      "/?layoutId=1&seatId=1&available=false&startTime=10:00&endTime=19:00&date=2021-12-15"
+    );
   };
 
   return (
     <div>
-      <button onClick={homeHandler}>링크 초기화 하기</button>
-      <button onClick={availableHandler}>예약 가능한 좌석</button>
-      <button onClick={unavailableHandler}>예약 불가능한 좌석</button>
+      <div>
+        <button>
+          <Link to={`${baseUrl}/`}>링크 초기화 하기</Link>
+        </button>
+        <button>
+          <Link
+            to={`${baseUrl}/?layoutId=1&seatId=1&available=true&startTime=10:00&endTime=19:00&date=2021-12-15`}
+          >
+            예약 가능한 좌석
+          </Link>
+        </button>
+        <button>
+          <Link
+            to={`${baseUrl}/?layoutId=1&seatId=1&available=false&startTime=10:00&endTime=19:00&date=2021-12-15`}
+          >
+            예약 불가능한 좌석
+          </Link>
+        </button>
+      </div>
+      <div>
+        <p>
+          <a href={`${baseUrl}/`}>test - home </a>
+        </p>
+        <p>
+          <a
+            href={`${baseUrl}/?layoutId=1&seatId=1&available=true&startTime=10:00&endTime=19:00&date=2021-12-15`}
+          >
+            test - 예약 가능한 좌석{" "}
+          </a>
+        </p>
+        <p>
+          <a
+            href={`${baseUrl}/?layoutId=1&seatId=1&available=false&startTime=10:00&endTime=19:00&date=2021-12-15`}
+          >
+            test - 예약 불가능한 좌석{" "}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
